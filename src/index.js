f0929fbb6eb6959f31185e379effbc2ca31d2671
drkspark <madhusudhanrock753@gmail.com>
2023-04-10 22:22:00 +0530

    Changed DB to Local

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const connect = require("./config/database");
 const { PORT } = require("./config/serverConfig");
-const Comment = require("./models/comment");
-const TweetRepository = require("./repository/tweet-repository");
 
 const app = express();
 
@@ -10,13 +8,4 @@ app.listen(PORT, async () => {
     console.log("Server Started");
     await connect();
     console.log("MongoDB Connected");
-
-    const tweetRepo = new TweetRepository();
-    // Now when we get our Tweet the Comments field is Storing the objId of the Comment and not the Comment data, we can populate it
-    // with the Comment data, by using autopopulate
-    // const tweet = await tweetRepo.getFew();
-    // console.log(tweet.length);
-    // const tweet = await tweetRepo.get("6433c746605a6a76687b53b9");
-    // console.log(tweet);
-    // console.log(tweet.contentWithEmail);
 });
