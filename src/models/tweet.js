6d54d5fb59d4dcbca96d8f99039d91e9b509cd8d
drkspark <madhusudhanrock753@gmail.com>
2023-04-11 21:37:50 +0530

    Added HashTag Model

--- src/models/tweet.js
+++ src/models/tweet.js
@@ -5,38 +5,18 @@ const tweetSchema = new mongoose.Schema(
         content: {
             type: String,
             required: [true, "Can't create tweet without content"],
+            // max: Maximum number of the character's in the tweet
+            max: [250, "Tweet can't be more than 250 characters"]
         },
-        userEmail: {
-            type: String,
-        },
-        date: {
-            type: String,
-        },
-        comments: [
+        hashtags: [
             {
                 type: mongoose.Schema.Types.ObjectId,
-                ref: "Comment",
-            },
-        ],
+                ref: "Hashtag"
+            }
+        ]
     },
     { timestamps: true }
 );
-
-tweetSchema.pre("save", function (next) {
-    let date = JSON.stringify(this.createdAt)
-        .substring(1, 11)
-        .split("-")
-        .reverse()
-        .join("-");
-
-    this.date = date;
-    next();
-});
-
-// Don't write arrow function () => {}, they don't have a context to the calling object i.e. they will give undefined
-tweetSchema.virtual("contentWithEmail").get(function process() {
-    return `${this.content} \nCreated By: ${this.userEmail}`;
-});
 
 const Tweet = mongoose.model("Tweet", tweetSchema);
 
